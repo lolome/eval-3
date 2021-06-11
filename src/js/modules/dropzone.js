997a662abb2ef4a4fb40ca47f85d0a5bc57a9345
Laurent Mercier <laurent@laurentmercier.fr>
2021-06-11 12:47:56 +0200

    WIP 2021-06-11 @ 12:47

--- src/js/modules/dropzone.js
+++ src/js/modules/dropzone.js
@@ -6,7 +6,6 @@
  */
 import Dropzone from 'dropzone';
 
-// ???
 // Make sure Dropzone doesn't try to attach itself to the
 // element automatically.
 // This behaviour will change in future versions.
@@ -18,5 +17,7 @@ Dropzone.autoDiscover = false;
  * - - - - - - - - - - - - - - - - - - - - -
  */
 document.addEventListener('DOMContentLoaded', () => {
-  //
+  const dropzoneForm1 = document.getElementById('dropzone-1');
+  if (!dropzoneForm1) return;
+  const dropzone1 = new Dropzone(dropzoneForm1, { url: '/file/post' });
 });
